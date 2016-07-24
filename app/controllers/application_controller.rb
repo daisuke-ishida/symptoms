ecf22ddc235a32fbf737d1681f995ed1b24ad151
daisukeishida <berg7295karajan@gmail.com>
2016-07-24 15:28:51 +0000

    error message created

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,14 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
   
+  class Frbidden < ActionController::ActionControllerError; end
+  class IpAddressRejected < ActionController::ActionControllerError; end
+    
+  rescue_from Exception, with: :rescue500
+  rescue_from Forbidden, with: :rescue403
+  rescue_from IpAddressRejected, with: :rescue403
+  rescue_from ActionController::RoutingError, with: :rescue404
+  
   private 
   def logged_in_user
     unless logged_in?
@@ -12,4 +20,20 @@ class ApplicationController < ActionController::Base
       redirect_to login_url
     end
   end
+  
+  def rescue404(e)
+    @exception = e
+    render 'errors/not_found', status: 404
+  end
+  
+  def rescue403(e)
+    @exception = e
+    render 'errors/forbidden', status: 403
+  end
+  
+  def rescue500(e)
+    @exception = e
+    render 'errors/internal_server_error', status: 500
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-
+  
   root to: 'static_pages#home'
   get 'signup', to: 'users#new'
   get 'login', to: 'sessions#new'
@@ -27,6 +27,7 @@ Rails.application.routes.draw do
   resources :posts
   resources :relationships, only: [:create, :destroy]
   resources :symptoms
+  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -82,4 +83,6 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  
+  get '*anything' => 'errors#routing_error'
 end
