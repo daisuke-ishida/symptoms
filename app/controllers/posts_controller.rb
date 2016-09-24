ca1e7b03b6364021534daa7659bd06bddbb9dad9
daisukeishida <berg7295karajan@gmail.com>
2016-09-24 02:26:33 +0000

    post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
     before_action :logged_in_user, only: [:create, :destroy, :followed]
-    before_action :correct_user, only: [:create, :destroy, :followed]
+    before_action :correct_user, only: [:destroy, :followed]
     before_action :check_timeout
     
     
